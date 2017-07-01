1b80f6c56ad9130ccd2625f6a2b82a8feb305231
lpelczar <lukepelczar@gmail.com>
2017-07-01 20:30:59 +0200

    1.1 Catalog Activity + Database

---
+++ app/src/main/java/pl/lukaszpelczar/p10_udacity_inventory/CatalogActivity.java
@@ -0,0 +1,180 @@
+package pl.lukaszpelczar.p10_udacity_inventory;/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import pl.lukaszpelczar.p10_udacity_inventory.data.ItemContract.ItemEntry;
+
+/**
+ * Displays list of items that were entered and stored in the app.
+ */
+public class CatalogActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+    /** Identifier for the item data loader */
+    private static final int ITEM_LOADER = 0;
+
+    /** Adapter for the ListView */
+    ItemCursorAdapter mCursorAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+
+        // Setup FAB to open DetailActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Find the ListView which will be populated with the item data
+        ListView itemListView = (ListView) findViewById(R.id.list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        itemListView.setEmptyView(emptyView);
+
+        // Setup an Adapter to create a list item for each row of item data in the Cursor.
+        // There is no item data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new ItemCursorAdapter(this, null);
+        itemListView.setAdapter(mCursorAdapter);
+
+        // Setup the item click listener
+        itemListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Create new intent to go to {@link DetailActivity}
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+
+                // Form the content URI that represents the specific item that was clicked on,
+                // by appending the "id" (passed as input to this method) onto the
+                // {@link ItemEntry#CONTENT_URI}.
+                // For example, the URI would be "content://pl.lukaszpelczar.p10_udacity_inventory/items/2"
+                // if the pet with ID 2 was clicked on.
+                Uri currentPetUri = ContentUris.withAppendedId(ItemEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentPetUri);
+
+                // Launch the {@link DetailActivity} to display the data for the current item.
+                startActivity(intent);
+            }
+        });
+
+        // Kick off the loader
+        getLoaderManager().initLoader(ITEM_LOADER, null, this);
+    }
+
+    /**
+     * Helper method to insert hardcoded pet data into the database. For debugging purposes only.
+     */
+    private void insertItem() {
+        ContentValues values = new ContentValues();
+        values.put(ItemEntry.COLUMN_ITEM_NAME, "Sluchawki");
+        values.put(ItemEntry.COLUMN_ITEM_QUANTITY, 234);
+        values.put(ItemEntry.COLUMN_ITEM_PRICE, 23);
+
+        // Insert a new row into the provider using the ContentResolver.
+        // Use the {@link ItemEntry#CONTENT_URI} to indicate that we want to insert
+        // into the items database table.
+        // Receive the new content URI that will allow us to access data in the future.
+        Uri newUri = getContentResolver().insert(ItemEntry.CONTENT_URI, values);
+    }
+
+    /**
+     * Helper method to delete all items in the database.
+     */
+    private void deleteAllItems() {
+        int rowsDeleted = getContentResolver().delete(ItemEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from item database");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                insertItem();
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                deleteAllItems();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                ItemEntry._ID,
+                ItemEntry.COLUMN_ITEM_NAME,
+                ItemEntry.COLUMN_ITEM_QUANTITY,
+                ItemEntry.COLUMN_ITEM_PRICE};
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                ItemEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update {@link ItemCursorAdapter} with this new cursor containing updated item data
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
+    }
+}
---
+++ app/src/main/java/pl/lukaszpelczar/p10_udacity_inventory/ItemCursorAdapter.java
@@ -0,0 +1,96 @@
+package pl.lukaszpelczar.p10_udacity_inventory;/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import pl.lukaszpelczar.p10_udacity_inventory.data.ItemContract.ItemEntry;
+
+import static pl.lukaszpelczar.p10_udacity_inventory.R.id.quantity;
+
+/**
+ * {@link ItemCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of item data as its data source. This adapter knows
+ * how to create list items for each row of item data in the {@link Cursor}.
+ */
+public class ItemCursorAdapter extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link ItemCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public ItemCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Inflate a list item view using the layout specified in list_item.xml
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the item data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current item can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find individual views that we want to modify in the list item layout
+        TextView nameTextView = (TextView) view.findViewById(R.id.product_name);
+        TextView quantityTextView = (TextView) view.findViewById(quantity);
+        TextView priceTextView = (TextView) view.findViewById(R.id.price);
+
+        // Find the columns of item attributes that we're interested in
+        int nameColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_NAME);
+        int quantityColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_QUANTITY);
+        int priceColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_PRICE);
+
+        // Read the item attributes from the Cursor for the current item
+        String itemName = cursor.getString(nameColumnIndex);
+        int itemQuantity = cursor.getInt(quantityColumnIndex);
+        int itemPrice = cursor.getInt(priceColumnIndex);
+
+        String quantityField = "Quantity: " + Integer.toString(itemQuantity);
+        String priceField = "Pride: " + Integer.toString(itemPrice);
+
+        // Update the TextViews with the attributes for the current item
+        nameTextView.setText(itemName);
+        quantityTextView.setText(quantityField);
+        priceTextView.setText(priceField);
+    }
+}
